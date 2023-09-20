99aa78eb150f38440568cdf23553f0a4626d5645
thienphuprogrammer <thienphuprogrammer@gmail.com>
2023-09-20 10:17:53 +0700

    update v3

--- src/BussinessLayer/Entity/Report/ReportProject.java
+++ src/BussinessLayer/Entity/Report/ReportProject.java
@@ -1,4 +1,21 @@
 package BussinessLayer.Entity.Report;
 
-public class ReportProject {
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReportProject extends Report {
+    private int project_id;
+
+    public ReportProject(int id, LocalTime time, LocalDate date, String description, int project_id) {
+        super(id, time, date, description);
+        this.project_id = project_id;
+    }
+
+    public int getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
 }
